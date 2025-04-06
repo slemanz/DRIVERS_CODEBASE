0affde294eb9d4b82b867f3f2d7277c1c20f4f84
slemanz <william_sleman@hotmail.com>
2025-04-06 09:00:54 -0300

    stm32f401(feat): add config gpio

--- STM32F401/App/Src/config.c
+++ STM32F401/App/Src/config.c
@@ -1,18 +1,18 @@
 #include "config.h"
 #include "stm32f401xx.h"
 
+static const GPIO_Config_t gpioConfigs[] = {
+    {GPIOB, GPIO_PIN_NO_2, GPIO_MODE_OUT, GPIO_SPEED_LOW, GPIO_OP_TYPE_PP, GPIO_NO_PUPD},
+    {GPIOA, GPIO_PIN_NO_5, GPIO_MODE_OUT, GPIO_SPEED_LOW, GPIO_OP_TYPE_PP, GPIO_NO_PUPD}
+    // Add more configurations as needed
+};
+
 static void config_gpio(void)
 {
-    GPIO_Handle_t GpioLed;
-	GpioLed.pGPIOx = GPIOB;
-	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_2;
-	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
-	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_LOW;
-	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
-	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
-
-    GPIO_Init(&GpioLed);
-    GPIO_WriteToOutputPin(GPIOB, GPIO_PIN_NO_2, GPIO_PIN_SET);
+    for (uint32_t i = 0; i < sizeof(gpioConfigs) / sizeof(gpioConfigs[0]); i++)
+    {
+        GPIO_Init((GPIO_Config_t *)&gpioConfigs[i]);
+    }
 }
 
 
@@ -21,5 +21,6 @@ void config_drivers(void)
     while(clock_init());
 
     config_gpio();
-    uart2_init();
+    systick_init(1000, clock_getValue());
+    //uart2_init();
 }
--- STM32F401/App/Src/main.c
+++ STM32F401/App/Src/main.c
@@ -1,12 +1,38 @@
 #include "config.h"
 #include "common-defines.h" 
 
+#include "driver_gpio.h"
+
+uint64_t ticks = 0;
+
+uint64_t ticks_get()
+{
+    return ticks;
+}
+
+
 int main(void)
 {
     config_drivers();
 
+    uint64_t start_time = ticks_get();
+
+    GPIO_WriteToOutputPin(GPIOB, GPIO_PIN_NO_2, GPIO_PIN_RESET);
+    GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_5, GPIO_PIN_SET);
+
     while (1)
     {
+        if((ticks_get() - start_time) >= 500)
+        {
+            GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+            GPIO_ToggleOutputPin(GPIOB, GPIO_PIN_NO_2);
+            start_time = ticks_get();
+        }
 
     }
+}
+
+void SysTick_Handler(void)
+{
+    ticks++;
 }
