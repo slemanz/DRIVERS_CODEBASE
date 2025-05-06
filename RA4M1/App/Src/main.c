94bf6296f0b33885437d3ec1bea98884691a6146
slemanz <william_sleman@hotmail.com>
2025-05-06 19:07:58 -0300

    r4am1(feat): add main file

---
+++ RA4M1/App/Src/main.c
@@ -0,0 +1,26 @@
+// Author: William Sleman @ 2025
+#include <stdint.h>
+
+#define GPIO_P0_BASEADDR        0x40040000U
+#define MMIO32(addr) (*(volatile uint32_t *)(addr))
+
+// Led   -> P111_GPT3_A
+const uint8_t leds_pin[] = {0, 6, 16, 24};
+
+void delay_cycles(uint32_t cycles)
+{
+    while (cycles-- > 0) {
+        __asm("NOP"); // No operation for delay
+    }
+}
+
+int main(void)
+ {
+    MMIO32(GPIO_P0_BASEADDR) |= (1 << 11); // set as output
+
+    while (1)
+    {   
+        MMIO32(GPIO_P0_BASEADDR) ^= (1 << 27); // toggle
+        delay_cycles(5000000);
+    }
+}
